5c4ced6cfb96524dcb66f7634c3fd3c0175cfb72
= <turbo_jordan@hotmail.com>
2017-04-14 22:22:57 +1200

    added person class

---
+++ BillManager/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillManager
+{
+    class Person
+    {
+
+        List<Bill> Personsbillslist = new List<Bill>();
+
+        public string _name { get; set; }
+
+        public decimal income { get; set; }
+
+        Person(string name,decimal income, List<Bill> listOfBills)
+        {
+
+        }
+
+
+    }
+}
